c8737bd33e9532a3cc9e0dea1223ef52815f4e76
Chandan1693 <77875662+Chandan1693@users.noreply.github.com>
2024-06-19 15:56:47 +0530

    Update googlespeech.py

--- googlespeech.py
+++ googlespeech.py
@@ -46,5 +46,5 @@ def download_file(filename):
     return send_file(filename, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8005)
+    app.run(host="0.0.0.0", port=8000)
 
